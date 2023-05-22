51fd291ae806ad582f895ee3b73fe084547ee95a
Julian Soriano <Julian Soriano@DESKTOP-M5NAJQU>
2023-05-22 17:41:12 -0500

    Metodo agregar

---
+++ ConsoleApp/Persona.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+
+
+    internal class Persona
+    {
+
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.IngresarDatos();
+        
+
+
+        }
+
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public Boolean genero { get; set; }
+
+        public  void IngresarDatos() {
+            bool ok = true;
+            do {
+                Console.WriteLine("Ingrese su nombre (Máximo 10 caracteres) :");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese su edad (Del 0 al 99) :");
+                
+                Console.WriteLine("Ingrese su género true=varon  false=mujer :");
+                nombre = Console.ReadLine();
+                ok = true;
+                Console.Read();
+
+            } while (ok);
+
+           
+        }
+        public void ParseInt(int edade) {
+            try {
+                edade = int.Parse(Console.ReadLine());
+            }
+            catch (ArgumentException e) {
+            
+            }
+            catch (FormatException e)
+            {
+
+            }
+           
+            catch (OverflowException e)
+            {
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+
+
+    }
+}
