aeb9201cad2e52d05cb117a77d555a9001aab559
Julian Soriano <Julian Soriano@DESKTOP-M5NAJQU>
2023-05-22 19:06:46 -0500

    actualizacion del metodo ingresardatos

--- ConsoleApp/Persona.cs
+++ ConsoleApp/Persona.cs
@@ -28,12 +28,23 @@ namespace ConsoleApp
         public int edad { get; set; }
         public bool genero { get; set; }
 
+        public bool errorok=false;
+
         public void IngresarDatos()
         {               
                 ParseString(nombre);
+                ComprobarFinalizar();
                 ParseInt(edad);
-                //Parsebool(genero);
-                Console.Read();
+                ComprobarFinalizar();
+                Parsebool(genero);
+                ComprobarFinalizar();
+            if (genero)
+            {
+                Console.WriteLine("Hola " + nombre + ", " + edad + " años, género masculino");
+            }else { 
+                Console.WriteLine("Hola " + nombre + ", " + edad + " años, género femenino"); 
+            }
+            Console.Read();
         }
         public void ParseInt(int edade)
         { bool ok = false;
@@ -49,34 +60,44 @@ namespace ConsoleApp
                         ok = true;
                     }
                 } while (ok);
+                edad = edade;
             }
             catch (IOException e)
             {
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
 
             }
             catch (OutOfMemoryException e)
             {
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (ArgumentOutOfRangeException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (ArgumentNullException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (FormatException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
 
             catch (OverflowException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (Exception e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
 
         }
@@ -89,25 +110,32 @@ namespace ConsoleApp
                     ok = false;
                     Console.WriteLine("Ingrese su nombre (Máximo 10 caracteres) :");
                     str = Console.ReadLine();
-                    if(str.Length>10 || str.Length <= 0)
+                    if(str.Length>10)
                     {
                         ok= true;
                     }
                 }while (ok);
+                nombre = str;
             }
             catch (IOException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (OutOfMemoryException e)
             {
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (ArgumentOutOfRangeException e)
             {
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (Exception e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
 
 
@@ -121,26 +149,46 @@ namespace ConsoleApp
                 Console.WriteLine("Ingrese su género true=varon  false=mujer :");
 
                 b = bool.Parse(Console.ReadLine());
+                genero = b;
             }
             catch (ArgumentNullException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:"+ e.Message);
+                errorok = true;
             }
             catch (FormatException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (IOException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch(OutOfMemoryException e)
             {
-
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
             }
             catch (ArgumentOutOfRangeException e)
             {
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocurrio un Error:" + e.Message);
+                errorok = true;
+            }
+        }
 
+        public void ComprobarFinalizar()
+        {
+            if (errorok)
+            {
+                Console.Read();
+                Environment.Exit(0);
             }
         }
     }
